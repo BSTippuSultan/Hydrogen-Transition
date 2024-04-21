579d94513055c6198c497c0ea562d82d10e54bf5
BSTippuSultan <bstippusultan@gmail.com>
2024-04-22 00:29:04 +0530

    updated

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let final_input = document.querySelector('#final');
 let initial_input = document.querySelector("#initial");
+
 let submit = document.querySelector('#submit');
 
 let energy_result = document.querySelector('#energy_result');
@@ -8,18 +9,54 @@ let eu = document.querySelector('#energy_units');
 let wavelength_result = document.querySelector("#wavelength_result");
 let wu = document.querySelector("#wavelength_units");
 
-submit.addEventListener('click',() => {
+let region = document.querySelector("#region");
+let color = document.querySelector("#color");
+
+let ans_e = 1 ; 
+let ans_w =  1 ;
+let ans_w_unit_nm ;
+
+eu.addEventListener('click',(event) => {
+    event.preventDefault();
+    calAns();
+})
+
+wu.addEventListener('click' , (event) => {
+    event.preventDefault();
+    calAns();
+})
+
+submit.addEventListener('click',(event) => {
+    event.preventDefault();
+    calAns();
+})
+function calAns (){
     let final = parseInt(final_input.value);
     let initial = parseInt(initial_input.value);
 
-    eu.value = 'J';
-    wu.value = 'nm';
+    if (eu.value == 'J') {
 
-    if(eu.value == 'J'){
-        let ans = 1*1;
-        ans = (-2.18)*(Math.pow((1/final),2)-Math.pow((1/initial),2));
-        console.log( ans );
-        energy_result.innerText = ans.toFixed(4) ;
+        ans_e = (-2.18) * (Math.pow(final , -2 ) - Math.pow(initial , -2 )) ;
+        energy_result.innerText = ans_e.toFixed(3) ;
+    } else if (eu.value == 'eV') {
+        ans_e = (13.6) * ((1/final)*(1/final) - (1/initial)*(1/initial)) ;
+        energy_result.innerText = ans_e.toFixed(3) ;
+    }  
+    
+    if (wu.value == 'nm') {
+        ans_w_unit_nm = true ;
+        ans_w = (100/1.097) * (Math.pow(final , -2 ) - Math.pow(initial , -2 ));
+        wavelength_result.innerText = ans_w.toFixed(3);
+    } else if (wu.value == 'm') {
+        ans_w_unit_nm = false ;
+        ans_w = (1/1.097) * (Math.pow(final , -2 ) - Math.pow(initial , -2 ));
+        wavelength_result.innerText = ans_w.toFixed(5);
     }
 
-})
+/*
+by using if else and variable ans_w_unit_nm = true ; we can assign remaining
+*/
+}
+
+
+
